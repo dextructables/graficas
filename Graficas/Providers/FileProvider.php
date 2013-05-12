b60c94c59eb2b98ca51536d0e1b25a3b9a19576e
Dex Barrett <dexter.x740@gmail.com>
2013-05-12 13:29:22 -0700

    se quito la variable redundante rowCounter

--- Graficas/Providers/FileProvider.php
+++ Graficas/Providers/FileProvider.php
@@ -12,15 +12,13 @@ class FileProvider implements DataInterface
 
     public function getData()
     {
-        $rowCounter = 0;
+
         $data       = array();
         $fileHandle = fopen($this->filePath, 'r');
 
         while (($serie = fgetcsv($fileHandle)) !== false) {
 
-            $data[$rowCounter] = array('titulo' => $serie[0], 'valores' => array_slice($serie,1));
-            $rowCounter++;
-  
+            $data[] = array('titulo' => $serie[0], 'valores' => array_slice($serie,1));
         }
 
         fclose($fileHandle);
